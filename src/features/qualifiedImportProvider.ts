21cdc45e4ca0f3652eccbe3640db08a933d03f16
Eduard Sergeev <eduard.sergeev@gmail.com>
2022-03-16 16:54:12 +1100

    Do not add unnecessary alias to qualified import
    Add `import qualified Say` instead of `import qualified Say as Say`
    for `Say.sayShow`

--- src/features/qualifiedImportProvider.ts
+++ src/features/qualifiedImportProvider.ts
@@ -22,7 +22,7 @@ export default class QualifiedImportProvider extends ImportProviderBase implemen
       const [, alias, name] = match;
 
       const results = await this.search(name);
-      codeActions = codeActions.concat(this.addImportForVariable(document, ` as ${alias}`, results));
+      codeActions = codeActions.concat(this.addImportForVariable(document, alias, results));
       codeActions.forEach(action => {
         action.diagnostics = [diagnostic];
       });
@@ -33,7 +33,7 @@ export default class QualifiedImportProvider extends ImportProviderBase implemen
   private addImportForVariable(document: TextDocument, alias: string, searchResults: SearchResult[]): CodeAction[] {
     const codeActions = [];
     for (const result of searchResults) {
-      const title = `Add: "import qualified ${result.module}${alias}"`;
+      const title = `Add: "import qualified ${result.module}${result.module !== alias ? ` as ${alias}` : ''}"`;
       const codeAction = new CodeAction(title, CodeActionKind.QuickFix);
       codeAction.command = {
         title: title,
@@ -42,7 +42,7 @@ export default class QualifiedImportProvider extends ImportProviderBase implemen
           document,
           result.module,
           {
-            alias: alias
+            alias: result.module !== alias ? ` as ${alias}` : null
           }
         ]
       };
